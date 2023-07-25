8810abc5a942df44210678642b908a727aa930db
koteczek <mew@koteczek.local>
2023-07-24 20:50:53 -0700

    update...

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,9 +2,53 @@
 
 public class MyBot : IChessBot
 {
+    // Piece values: null, pawn, knight, bishop, rook, queen, king
+    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move[] allMoves = board.GetLegalMoves();
+
+        // Pick a random move to play if nothing better is found
+        System.Random rng = new();
+        Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
+        int highestValueCapture = 0;
+
+        foreach (Move move in allMoves)
+        {
+            // Always play checkmate in one
+            if (MoveIsCheckmate(board, move))
+            {
+                moveToPlay = move;
+                break;
+            }
+
+            // Find highest value capture
+            Piece capturedPiece = board.GetPiece(move.TargetSquare);
+            int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
+
+            if (capturedPieceValue > highestValueCapture)
+            {
+                moveToPlay = move;
+                highestValueCapture = capturedPieceValue;
+            }
+        }
+
+        return moveToPlay;
     }
+
+    // Test if this move gives checkmate
+    bool MoveIsCheckmate(Board board, Move move)
+    {
+        board.MakeMove(move);
+        bool isMate = board.IsInCheckmate();
+        board.UndoMove(move);
+        return isMate;
+    }
+    // public Move Think(Board board, Timer timer)
+    // {
+    //     Move[] moves = board.GetLegalMoves();
+    //     System.Random rng = new();
+    //     return moves[rng.Next(moves.Length)];
+    // }
 }
